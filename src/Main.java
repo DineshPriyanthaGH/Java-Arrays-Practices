c8126ed4ec641f642b7ac80b72bbc692d22197ce
Dinesh Priyantha <dineshpriyantha200248@gmail.com>
2024-02-28 22:58:30 +0530

    single Dimension arrays

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+     //single dimension array
+     String[] names;
+     names =new String[5];
+     //initialization
+        int[] marks = {100,200,300};
+        System.out.println(Arrays.toString(marks));
+        int[] marks2={2,2,3,21,1,2,123,3};
+        System.out.println(Arrays.toString(marks2));
+        }
+    }
